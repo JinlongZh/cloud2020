31180f9f0d72e4d59ad3bd246364930ceb3499ec
zjl <2994381317@qq.com>
2022-05-04 17:45:50 +0800

    after Ribbon

--- cloud-consumer-order80/src/main/java/com/gangu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/gangu/springcloud/controller/OrderController.java
@@ -2,13 +2,19 @@ package com.gangu.springcloud.controller;
 
 import com.gangu.springcloud.entities.CommonResult;
 import com.gangu.springcloud.entities.Payment;
+import com.gangu.springcloud.lb.LoadBalancer;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -17,20 +23,52 @@ public class OrderController {
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
     @Resource
+    private LoadBalancer iLoadBalancer;
+    @Resource
+    private DiscoveryClient discoveryClient;
+
+    @Resource
     private RestTemplate restTemplate;
 
     //因为浏览器只支持get请求，为了方便这里就用get
     @GetMapping("/consumer/payment/create")
-    public CommonResult<Payment> create(Payment payment){
+    public CommonResult<Payment> create(Payment payment) {
         log.info("********插入的数据：" + payment);
         //postForObject分别有三个参数：请求地址，请求参数，返回的对象类型
         return restTemplate.postForObject(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
     }
 
     @GetMapping("/consumer/payment/get/{id}")
-    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id) {
         log.info("********查询的id：" + id);
         //getForObject两个参数：请求地址，返回的对象类型
         return restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment2(@PathVariable("id") Long id) {
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+
+    //手写轮训算法
+    @GetMapping("/consumer/payment/lb")
+    public String getPaymentLB(){
+        //获取CLOUD-PAYMENT-SERVICE服务的所有具体实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if(instances == null || instances.size() <= 0){
+            return null;
+        }
+
+        ServiceInstance serviceInstance = iLoadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+        System.out.println(uri);
+
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/gangu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/gangu/springcloud/controller/PaymentController.java
@@ -42,4 +42,9 @@ public class PaymentController {
         }
     }
 
+    @GetMapping("/payment/lb")
+    public String getPaymentLB(){
+        return serverPort;
+    }
+
 }
