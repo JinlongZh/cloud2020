c67e46a3938def2ec113b1f6a4f5fd4311c56d3d
zjl <2994381317@qq.com>
2022-05-09 17:16:57 +0800

    after Bus

---
+++ cloud-config-client-3366/src/main/java/com/gangu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,13 @@
+package com.gangu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+
+}
